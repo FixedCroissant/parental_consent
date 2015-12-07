1d3e8d26c3b2f914e3547befc39393763b9bc771
Joshua Williams <jjwill10@ncsu.edu>
2015-12-07 09:02:06 -0500

    Initial commit of my new class to check database pull functions and to see if there are any matches on the front page before moving the second page of the parental consent web-application
    The test_connection_and_class is a test file to check the functionality
    of this new class before it's implemented into the web-application.

---
+++ classes/pullData.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jjwill10
+ * Date: 12/7/2015
+ * Time: 8:40 AM
+ */
+
+class pullData {
+
+    //Variables
+
+    var $parentalEMail;
+    var $studentDOB;
+    var $housingAgreementSubmission;
+
+
+
+
+        function provideConnection(){
+
+
+        }
+
+
+        function IsMatch(){
+
+
+        }
+
+
+
+}
---
+++ test_connection_and_class.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jjwill10
+ * Date: 12/7/2015
+ * Time: 8:59 AM
+ */
